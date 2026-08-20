b64dde5c06a6348d4325ef6b0e1757948e84c036
ra7-cyber6565 <ravibarupal007@gmail.com>
2026-08-20 21:22:26 +0530

    Add storage provider package

---
+++ storage/__init__.py
@@ -0,0 +1,5 @@
+"""Storage provider abstractions for Infinity Research AI."""
+
+from .cloud_archive import ArchiveService, CloudObject, CloudStorageProvider
+
+__all__ = ["ArchiveService", "CloudObject", "CloudStorageProvider"]
